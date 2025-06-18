d4c52dd314b7bf57b40b794e62e06cc76c76dc81
Bit-Inside <bondarenkoyana555@gmail.com>
2025-06-18 16:24:45 +0200

    Completed task1: calculate days from today

---
+++ task1.py
@@ -0,0 +1,12 @@
+
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        input_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        delta = today - input_date
+        return delta.days
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте YYYY-MM-DD"
+print (get_days_from_today("2025-05-28"))
